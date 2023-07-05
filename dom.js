75d88a9d90bf79a1769f1d93d019c16fd1f3bd90
ut02 <utsavsimplecool@gmail.com>
2023-07-05 16:14:16 +0530

    first commit

---
+++ dom.js
@@ -0,0 +1,17 @@
+//console.log("helloworld")
+//console.log("hii")
+//console.dir(document)
+//console.log(document.URL)
+//console.log(document.title)
+//document.title=123
+//console.log(document.doctype)
+//console.log(document.head)
+//console.log(document.body)
+//console.log(document.all)
+//console.log(document.forms)
+console.log(document.getElementById('main-header'))
+var header_title=document.getElementById('main-header')
+//console.log(header_title)
+//header_title.textContent='hello'
+header_title.innerText='goodbye'
+header_title.style.borderBottom='solid 3px #000'
